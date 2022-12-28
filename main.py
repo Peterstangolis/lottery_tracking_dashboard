eda2d710828fe7eda659ac628880acd3c05b0c9b
Peter Stangolis <pstangolis@gmail.com>
2022-12-28 13:06:54 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+## Import Libraries
+import random
+from random import choice
+
+import datetime as dt
+
+import requests
+from bs4 import BeautifulSoup
+
+import re
+
+import pandas as pd
+
+
+
+
+
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
