53a0ac743c82add61fa9555fd83e8a12380c2582
Peter Stangolis <pstangolis@gmail.com>
2022-12-28 13:09:09 -0500

    initial commit

---
+++ variables.py
@@ -0,0 +1,28 @@
+url_lottery_guru = 'https://lotteryguru.com/ontario-lottery-results'
+
+
+
+list_of_lotteries = {
+    "Lotto 6/49": {"selections": 6,
+                   "range_of_nums": [1, 49]
+                   },
+
+    "Lotto Max": {"selections": 7,
+                  "range_of_nums": [1, 50]
+                  },
+    "Daily Grand": {"selections": [5, 1],
+                    "range_of_numbs": [[1, 49], [1, 7]]
+                    },
+    "Daily Keno": {"selections": 20,
+                   "range_of_nums": [1, 70],
+                   "pick_category": [2, 3, 4, 5, 6, 7, 8, 9, 10],
+                   "bet_amount": [1.00, 2.00, 5.00, 10.00],
+                   "draw_occurence": "daily",
+                   "draw_time": ["14:00", "22:30"],
+                   "time_zone": "Eastern Time"
+                   },
+    "Ontario 49": {"selections": 6,
+                   "range_of_nums": [1, 40]
+                   }
+}
+
